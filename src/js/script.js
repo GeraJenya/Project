70f7b6999d48b643fdbc43bff9ec51e374f232ee
Yevhen <Gera5075@icloud.com>
2020-06-12 23:54:06 +0300

    1206 commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true
-      });
-  });
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',   
+        });
+    });
+    
+    
